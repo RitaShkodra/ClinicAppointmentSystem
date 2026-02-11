3e2dbf1d1b4d60b709b4e0a79b0085b7d50894d5
Rita Shkodra <ritashkodra@gmail.com>
2026-02-11 22:45:18 +0100

    aded JWT auth middleware and role based protection

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -10,3 +10,11 @@ router.post("/register", register);
 router.post("/login", login);
 
 export default router;
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+router.get("/me", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
